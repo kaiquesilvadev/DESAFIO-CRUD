ce3e4f9bcc2dde1121a2f15b753aff991f06467d
kaiquesilvadev <j.kaique1570@gmail.com>
2023-12-22 18:51:58 -0300

    first commit

---
+++ src/test/java/com/kaique/DesafioCrud/DesafioCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaique.DesafioCrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
